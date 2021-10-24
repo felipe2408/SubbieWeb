280321e66a030c05a427b81400ccfc80219c985d
Pipe <brianfelipe2408@gmail.com>
2021-10-24 16:56:44 -0500

    se agregan mejoras de rendimiento en el login para iniciar sesion

---
+++ SubbieWeb/Filters/VerificarSession.cs
@@ -0,0 +1,41 @@
+﻿using SubbieWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Utilitarios;
+namespace SubbieWeb.Filters
+{
+    public class VerificarSession : ActionFilterAttribute
+    {
+        private UUsuarios oUsuario;
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+
+            try {
+                base.OnActionExecuted(filterContext);
+                oUsuario = (UUsuarios)HttpContext.Current.Session["User"];
+                if (oUsuario == null)
+                {
+                    if (filterContext.Controller is SplashController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("/Splash/Login");
+                    }
+                }
+
+
+            }
+            catch (Exception)
+            {
+                filterContext.Result = new RedirectResult("~/Splash/Login");
+            }
+
+
+
+
+        }
+
+    }
+}
